62fd7c007f8ed784c81feec5ec5f73be521c4088
Esteban Reyes Marcelo <e.reyesm@uniandes.edu.co>
2023-05-13 12:05:27 -0500

    E2E v2 all test

--- e2e cypress ghost test/cypress/objects/homePage.js
+++ e2e cypress ghost test/cypress/objects/homePage.js
@@ -16,3 +16,22 @@ export class HomePage {
     }
 
 }
+
+export class HomePageV2 {
+
+    constructor() {
+    }
+
+    navigateModule(module) {
+        cy.get('a[href="#/' + module + '/"]').then($modules => {
+            cy.wrap($modules.get(0)).click({ force: true });
+        });
+    }
+
+    navigateModuleDetail(module) {
+        cy.get('a[href="#/' + module + '"]').then($modules => {
+            cy.wrap($modules.get(0)).click({ force: true });
+        });
+    }
+
+}
--- e2e cypress ghost test/cypress/objects/memberPage.js
+++ e2e cypress ghost test/cypress/objects/memberPage.js
@@ -44,3 +44,47 @@ export class MemberPage {
     }
     
 }
+
+export class MemberPageV2 {
+
+    constructor() {
+    }
+
+    activateSectionMembers(){
+        utility.clickInOptionAction('.gh-setting-action');
+    }
+
+    activateOptionMembers(){
+        utility.activateCheckBox();
+    }
+
+    save(){
+        utility.clickButtonSave('button.gh-btn')
+    }
+
+    fillForm(selector, value){
+        utility.enterInputInForm(selector, value);
+    }
+
+    findInList(selectorSection, selectorFind, dataFakerFind) {
+        utility.findInListSection(selectorSection, selectorFind, dataFakerFind);
+    }
+
+    findInList(selectorSection, selectorFind, dataFakerFind) {
+        utility.findInListSection(selectorSection, selectorFind, dataFakerFind);
+    }
+    
+    validateErrorMessage(selector) {
+        cy.get(selector).then((section) => {
+            if (section.length > 0) {
+                var errorMessageEmail = section.get(1);
+                if (!Cypress.dom.isHidden(errorMessageEmail)) {
+                    cy.wrap(true);
+                } else {
+                    return false;
+                }
+            }
+        });
+    }
+    
+}
