3b0b9d37e915a6fa2b641cb524dbe83a5a33d3b7
f-munozg <f.munozg@uniandes.edu.co>
2023-05-12 21:01:41 -0500

    modify report funcionality

--- RegressionTest Kraken/Index.js
+++ RegressionTest Kraken/Index.js
@@ -5,8 +5,17 @@ const fs = require('fs');
 const { options } = config;
 
 async function executeTest(){
-  let pathAfter = './results/after'
-  let pathBefore = './results/before'
+  let features = [
+    { "feature" : "CreatePage", "before" : "CreatePage/Escenario_2", "after": "CreatePage_V4/Escenario_2"},
+    { "feature" : "CreatePost", "before" : "CreatePost/Escenario_2", "after" : "CreatePost_V4/Escenario_2"},
+    { "feature" : "CreateTags", "before" : "CreateTags/Escenario_1",  "after":"CreateTags_V4/Escenario_1"},
+    { "feature" : "EditPost", "before" : "EditPost/Escenario_1",  "after":"EditPost_V4/Escenario_1"},
+    { "feature" : "Navegacion", "before" : "Navegacion/Escenario_1",  "after":"Navegacion_V4/Escenario_1"}
+  ]
+
+  features.forEach(async feature => {
+  let pathAfter = './results/'+ feature["after"]
+  let pathBefore = './results/' + feature["before"]
   var filesBefore = fs.readdirSync(pathBefore)
   var filesAfter = fs.readdirSync(pathAfter)
     if(filesBefore.length === 0 && filesAfter.length === 0){
@@ -31,15 +40,16 @@ async function executeTest(){
             diffBounds: data.diffBounds,
             analysisTime: data.analysisTime
         }
-        fs.writeFileSync(`./results/report/compare-${b}`, data.getBuffer());
+        fs.writeFileSync(`./results/report/${feature["feature"]}/compare-${b}`, data.getBuffer());
     }
 
-    fs.writeFileSync(`./results/report/report.html`, createReport(datetime, resultInfo));
-    fs.copyFileSync('./index.css', `./results/report/index.css`);
+    fs.writeFileSync(`./results/report/${feature["feature"]}/report.html`, createReport(datetime, resultInfo));
+    fs.copyFileSync('./index.css', `./results/report/${feature["feature"]}/index.css`);
 
     console.log('------------------------------------------------------------------------------------')
     console.log("Execution finished. Check the report under the results folder")
     return resultInfo;  
+    });
   }
 
   function browser(b, info){
